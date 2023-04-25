aa7a80a871a579154a45ab1380a884229a2659e4
mr2005 <cenrugi@gmail.com>
2023-04-25 06:24:01 +0200

    Portal inmobiliario

--- src/main.js
+++ src/main.js
@@ -1,6 +1,27 @@
 import { createApp } from 'vue'
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
 
-createApp(App).use(store).use(router).mount('#app')
+//import * as Vue from 'vue' // in Vue 3
+
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+
+
+// Agregamos la URL base de nuestra API
+axios.defaults.baseURL = 'http://localhost:3000/api/';
+
+
+loadFonts()
+
+createApp(App)
+  .use(router)
+  .use(store)
+  .use(vuetify)
+  .use(VueAxios, axios)
+  .mount('#app')
---
+++ src/views/MispropiedesView.vue
@@ -0,0 +1,13 @@
+<template lang="">
+    <div>
+        <h1>Mis Propiedades</h1>
+    </div>
+</template>
+
+<script>
+export default{
+    mounted(){
+        this.axios.get()
+    }
+}
+</script>
