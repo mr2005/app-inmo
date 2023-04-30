3683b1e8f2b676ca37a1052cc46c3598777107e9
mr2005 <cenrugi@gmail.com>
2023-04-30 07:17:23 +0200

    add in component hijo

--- src/components/CrearPropiedad.vue
+++ src/components/CrearPropiedad.vue
@@ -1,70 +1,89 @@
 <template>
-    <v-container justify-center>
-      <v-row>
-          <v-col class="mb-1">
-              <v-dialog
-              v-model="dialog"
-              persistent
-              width="1024"
-              >
-              <template v-slot:activator="{ props }">
-                  <v-btn  class="mx-2" fab dark color="primary" v-bind="props">
-                  <v-icon dark size="large">mdi-home-plus</v-icon>
-              </v-btn>
-              </template>
-              <v-card>
-      <v-card-title>
-        <span class="text-h5">Agregar propiedad</span>
-      </v-card-title>
-        <v-card-text>
-          <v-container>
-            <v-row>
-              <v-col cols="12" sm="6" md="4">
-                <v-text-field v-model="propiedad.titulo" label="Titulo descriptivo*" hint="Titulo de la propiedad" required></v-text-field>
-              </v-col>
-              <v-col cols="12" sm="6" md="4">
-                <v-text-field v-model="propiedad.precio" label="Precio de venta*" required></v-text-field>
-              </v-col>
-              <v-col cols="12" sm="6" md="4">
-                <v-text-field v-model="propiedad.ano" label="Año de construción"></v-text-field>
-              </v-col>
-              <v-col cols="12" sm="6" md="4">
-                <v-autocomplete
-                v-model="propiedad.categoria"
-                clearable
-                label="Categoría*"
-                :items="['Casa', 'Piso', 'Garaje', 'Terreno']">
-                required
-              </v-autocomplete>
+  <v-row class="ml-10">
+    <v-dialog v-model="dialog" persistent  width="1024"
+                >
+                <template v-slot:activator="{ props }">
+                    <v-btn  class="mx-2" fab dark color="primary" v-bind="props">
+                    <v-icon dark size="large">mdi-home-plus</v-icon>
+                </v-btn>
+                </template>
+                <v-card>
+        <v-card-title>
+          <span class="text-h5">Agregar propiedad</span>
+        </v-card-title>
+          <v-card-text>
+            <v-container>
+              <v-row>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field v-model="propiedad.titulo" label="Titulo descriptivo*" hint="Titulo de la propiedad" required></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field v-model="propiedad.precio" label="Precio de venta*" required></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field v-model="propiedad.ano" label="Año de construción"></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="4">
+                  <v-autocomplete
+                  v-model="propiedad.categoria"
+                  clearable
+                  label="Categoría*"
+                  :items="['Casa', 'Piso', 'Garaje', 'Terreno']">
+                  required
+                </v-autocomplete>
 
-              </v-col>
-              
-              <v-col sm="12" md="8">
-                <v-text-field v-model="propiedad.descripcion" label="Descripción de la propiedad" hint="Información descriptiva de la propiedad" persistent-hint></v-text-field>
-              </v-col>
-          
+                </v-col>
+                
+                <v-col sm="12" md="8">
+                  <v-text-field v-model="propiedad.descripcion" label="Descripción de la propiedad" hint="Información descriptiva de la propiedad" persistent-hint></v-text-field>
+                </v-col>
             
-            </v-row>
-          </v-container>
-          <small>*Campos requeridos</small>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="primary" variant="text" @click="dialog = false">Cerrar</v-btn>
-          <v-btn color="primary" variant="text" @click="agregarPropiedad()">Añadir</v-btn>
-        </v-card-actions>
-      </v-card>
-              </v-dialog>
-          </v-col>
-        </v-row>  
-    </v-container>
-  </template>
+              
+              </v-row>
+            </v-container>
+            <small>*Campos requeridos</small>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" variant="text" @click="dialog = false">Cerrar</v-btn>
+            <v-btn color="primary" variant="text" @click="agregarPropiedad()">Añadir</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+  </v-row>
+</template>
 
 <script>
-export default {
-  name: 'CrearPropiedad',
-  data: () => ({
-    dialog: true
-  })
-}
+  export default {
+    data: () => ({
+      dialog: false,
+
+      propiedad: {
+              titulo: '',
+              precio: '',
+              ano: '',
+              categoria: '',
+              descripcion: '',
+  }
+}),
+    
+    agregarPropiedad() {
+      var router = this.$router;
+        const formData = new FormData();
+        formData.append('titulo',this.propiedad.titulo);
+        formData.append('precio',this.propiedad.precio);
+        formData.append('ano',this.propiedad.ano);
+        formData.append('categoria',this.propiedad.categoria);
+        formData.append('descripcion',this.propiedad.descripcion);
+        axios.post('http://localhost:3000/api/propiedades',formData)
+        .then(()=>{
+            router.push('/propiedades');
+        })
+        .catch(function(error){
+    console.log(error);
+        });
+
+      },
+
+  }
 </script>
--- src/components/ListarPropiedades.vue
+++ src/components/ListarPropiedades.vue
@@ -7,59 +7,7 @@
         </v-row>
         <v-row>
           <v-col class="mb-1">
-              <v-dialog
-              v-model="dialog"
-              persistent
-              width="1024"
-              >
-              <template v-slot:activator="{ props }">
-                  <v-btn  class="mx-2" fab dark color="primary" v-bind="props">
-                  <v-icon dark size="large">mdi-home-plus</v-icon>
-              </v-btn>
-              </template>
-              <v-card>
-      <v-card-title>
-        <span class="text-h5">Agregar propiedad</span>
-      </v-card-title>
-        <v-card-text>
-          <v-container>
-            <v-row>
-              <v-col cols="12" sm="6" md="4">
-                <v-text-field v-model="propiedad.titulo" label="Titulo descriptivo*" hint="Titulo de la propiedad" required></v-text-field>
-              </v-col>
-              <v-col cols="12" sm="6" md="4">
-                <v-text-field v-model="propiedad.precio" label="Precio de venta*" required></v-text-field>
-              </v-col>
-              <v-col cols="12" sm="6" md="4">
-                <v-text-field v-model="propiedad.ano" label="Año de construción"></v-text-field>
-              </v-col>
-              <v-col cols="12" sm="6" md="4">
-                <v-autocomplete
-                v-model="propiedad.categoria"
-                clearable
-                label="Categoría*"
-                :items="['Casa', 'Piso', 'Garaje', 'Terreno']">
-                required
-              </v-autocomplete>
-
-              </v-col>
-              
-              <v-col sm="12" md="8">
-                <v-text-field v-model="propiedad.descripcion" label="Descripción de la propiedad" hint="Información descriptiva de la propiedad" persistent-hint></v-text-field>
-              </v-col>
-          
-            
-            </v-row>
-          </v-container>
-          <small>*Campos requeridos</small>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="primary" variant="text" @click="dialog = false">Cerrar</v-btn>
-          <v-btn color="primary" variant="text" @click="agregarPropiedad()">Añadir</v-btn>
-        </v-card-actions>
-      </v-card>
-              </v-dialog>
+            <CrearPropiedad />
           </v-col>
         </v-row>  
         <v-row class="text-center">
@@ -89,9 +37,15 @@
 </template>
 
 <script>
+import CrearPropiedad from '../components/CrearPropiedad.vue';
 import axios from 'axios';
-export default {    
+
+export default {
+   
     name:'ListarPropiedades',
+    components: { 
+      CrearPropiedad 
+    }, 
     mounted(){
         this.obtenerPropiedades();
     },
@@ -99,17 +53,10 @@ export default {
         return{            
             propiedades: [],
             dialog: false,
-
-            propiedad: {
-              titulo: '',
-              precio: '',
-              ano: '',
-              categoria: '',
-              descripcion: '',
             }
               
         }
-    },
+    ,
     methods:{
         obtenerPropiedades(){
             axios.get('http://localhost:3000/api/propiedades')
@@ -122,24 +69,7 @@ export default {
             })
 
         },
-        /*
-        agregarPropiedad() {
-          var router = this.$router;
-           const formData = new FormData();
-           formData.append('titulo',this.propiedad.titulo);
-           formData.append('precio',this.propiedad.precio);
-           formData.append('ano',this.propiedad.ano);
-           formData.append('categoria',this.propiedad.categoria);
-           formData.append('descripcion',this.propiedad.descripcion);
-           axios.post('http://localhost:3000/api/propiedades',formData)
-           .then(()=>{
-               router.push('/propiedades');
-           })
-           .catch(function(error){
-        console.log(error);
-            });
 
-      },*/
     }
 }
 </script>
