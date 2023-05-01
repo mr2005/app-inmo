1aff0b38f21c54d2caedec83018438a4d5529eb3
mr2005 <cenrugi@gmail.com>
2023-05-01 09:30:44 +0200

    edit form in progress

--- src/components/EditarPropiedad.vue
+++ src/components/EditarPropiedad.vue
@@ -1,45 +1,40 @@
 <template>
-  <v-form v-model="valid">
+  <v-form v-model="formedic">
     <v-container>
+      <v-row class="text-center">
+          <v-col class="mb-4">
+              <h1>Edición de propiedad</h1>
+          </v-col>
+        </v-row>
       <v-row>
-        <v-col
-          cols="12"
-          md="4"
-        >
-          <v-text-field
-            v-model="firstname"
-            :rules="nameRules"
-            :counter="10"
-            label="First name"
-            required
-          ></v-text-field>
+        <v-col cols="12" sm="6" md="4">
+          <v-text-field label="Titulo descriptivo*" hint="Titulo de la propiedad" required></v-text-field>
         </v-col>
 
-        <v-col
-          cols="12"
-          md="4"
-        >
-          <v-text-field
-            v-model="lastname"
-            :rules="nameRules"
-            :counter="10"
-            label="Last name"
-            required
-          ></v-text-field>
+        <v-col cols="12" sm="6" md="4">
+          <v-text-field  label="Precio de venta*" required></v-text-field>
         </v-col>
-
-        <v-col
-          cols="12"
-          md="4"
-        >
-          <v-text-field
-            v-model="email"
-            :rules="emailRules"
-            label="E-mail"
-            required
-          ></v-text-field>
+        <v-col cols="12" sm="6" md="4">
+          <v-text-field  label="Año de construción"></v-text-field>
+        </v-col>
+        <v-col cols="12" sm="6" md="4">
+          <v-autocomplete
+                  clearable
+                  label="Categoría*"
+                  :items="['Casa', 'Piso', 'Garaje', 'Terreno']">
+                  required
+                </v-autocomplete>
+        </v-col>
+        <v-col sm="12" md="8">
+          <v-text-field label="Descripción de la propiedad" hint="Información descriptiva de la propiedad" persistent-hint></v-text-field>
+        </v-col>
+        <v-spacer></v-spacer>
+        <v-col class="text-center m">
+          <v-btn class="me-4">Actualizar</v-btn>
         </v-col>
       </v-row>
+      
+
     </v-container>
   </v-form>
 </template>
